f8cf164ac920a2cbb1c782659c812e5187b9e0b3
Ron Tseytlin <ronts390@gmail.com>
2023-11-12 22:50:01 +0200

    blank window

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,71 @@
-fn main() {}
+use std::error::Error;
+
+use error_iter::ErrorIter as _;
+use log::error;
+use pixels::{Pixels, SurfaceTexture};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+use winit_input_helper::WinitInputHelper;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let event_loop = EventLoop::new().unwrap();
+    let mut input = WinitInputHelper::new();
+
+    const WIDTH: u32 = 400;
+    const HEIGHT: u32 = 300;
+
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let scaled_size = LogicalSize::new(WIDTH as f64 * 2.0, HEIGHT as f64 * 2.0);
+        WindowBuilder::new()
+            .with_title("3dgame")
+            .with_inner_size(scaled_size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    event_loop.run(move |event, elwt| {
+        match event {
+            Event::WindowEvent {
+                ref event, ..
+            } => match event {
+                WindowEvent::RedrawRequested => {
+                    if let Err(err) = pixels.render() {
+                        log_error("pixels.render", err);
+                        elwt.exit();
+                        return;
+                    }
+                }
+                WindowEvent::CloseRequested => {
+                    elwt.exit();
+                }
+                _ => (),
+            },
+            _ => (),
+        }
+
+        if input.update(&event) {
+            window.request_redraw();
+        }
+    })?;
+
+    Ok(())
+}
+
+fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
+    error!("{method_name}() failed: {err}");
+    for source in err.sources().skip(1) {
+        error!("  Caused by: {source}");
+    }
+}
